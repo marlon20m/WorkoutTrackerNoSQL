57d0c218e6f731a190cd5c3440b5da433c03a2d0
marlon20m <marlonmora.ndr@gmail.com>
2020-08-20 12:56:20 -0500

    server.js routes/api routes/html models/index models/workout

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+// https://expressjs.com/en/api.html#express.urlencoded
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// routes
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
